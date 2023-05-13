8d93190e2946260b396aa4a4474b987cc00c7f90
Bearlingian <manuel.ramon.rangel.luna@gmail.com>
2023-05-13 16:42:04 -0500

    Commit Inicial

---
+++ src/main/java/com/pragma/powerup/usermicroservice/MicroPersonasApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.powerup.usermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPersonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPersonasApplication.class, args);
+	}
+
+}
